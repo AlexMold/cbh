41603f1c3572da5b26406279ca993c23efdd2037
Alexandr Gribkov <alexandr.gribkov@yopeso.com>
2015-12-01 14:59:01 +0200

    account page

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
+'use strict';
+
+
 var gulp = require('gulp'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
     notify = require("gulp-notify"),
     bower = require('gulp-bower'),
-    pixrem = require('gulp-pixrem');
+    autoprefix = require('gulp-autoprefixer'),
+    imageop = require('gulp-image-optimization');
 
 var config = {
     sassPath: './resources/sass',
@@ -14,6 +18,15 @@ gulp.task('bower', function() {
         .pipe(gulp.dest(config.bowerDir)) 
 });
 
+gulp.task('images', function(cb) {
+    gulp.src(['resources/**/*.png','resources/**/*.jpg','resources/**/*.gif','resources/**/*.jpeg'])
+    .pipe(imageop({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    })).pipe(gulp.dest('public')).on('end', cb).on('error', cb);
+});
+
 gulp.task('icons', function() {
     return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
         .pipe(gulp.dest('./public/fonts'));
@@ -21,6 +34,7 @@ gulp.task('icons', function() {
 
 gulp.task('css', function() {
     return gulp.src(config.sassPath + '/style.scss')
+        // .pipe(autoprefix())
         .pipe(sass()
             .on("error", notify.onError(function (error) {
                 return "Error: " + error.message;
@@ -28,15 +42,10 @@ gulp.task('css', function() {
         .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('pxrem', function() {
-    return gulp.src('public/css/style.css')
-        .pipe(pixrem({ rootValue: '10px' }))
-        .pipe(gulp.dest('./public/css/main'));
-});
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
     gulp.watch(config.sassPath + '/**/*.scss', ['css']);
 });
 
-gulp.task('default', ['bower', 'icons', 'css']);
+gulp.task('default', ['bower', 'icons', 'css', 'images']);
