91959074920aee0ba6757770c33798c29be902be
AlexMold <mangol72@gmail.com>
2015-12-13 10:48:09 +0200

    make a order

--- public/js/common.js
+++ public/js/common.js
@@ -1,15 +1,20 @@
+// **** JQuery add independent triggers and events *****
+
 $("#menu-toggle").click(function(e) {
 		e.preventDefault();
 		$(".wrapper, .sidebar-wrapper, .info-box__trigger, .info-box").toggleClass("toggled");
 });
+
 $(".header-card__trigger").click(function(e) {
 		e.preventDefault();
 		$(this).parent('.header-card').toggleClass("toggled");
 });
+
 $(".dicom-options__head").click(function(e) {
 		e.preventDefault();
 		$(this).siblings('.dicom-options__list').toggle('hidden');
 });
+
 $(".mail-filter__item").click(function(e) {
 		e.preventDefault();
 		$(this).toggleClass('active');
@@ -28,6 +33,68 @@ $(".select-item").click(function(e) {
 		$(this).parent('ul').toggle('hidden');
 });
 
+$('.row-phone .js__delete-row').on('click', function(){
+	$(this).parent('.row-phone').hide();
+});
+
+$('.js__add-row').on('click', function(e){
+	e.preventDefault();
+	// var innerRow = $(this).parent().parent($('.row-phone'));
+	var innerH = $('.fieldset').append($('.row-phone').html());
+});
+
+$(".info-box__trigger").click(function(e) {
+		e.preventDefault();
+		$(this).siblings('.info-box').removeClass('hidden');
+});
+
+$(".js-call__popup").click(function(e) {
+		e.preventDefault();
+		$(this).siblings('.cms__widget-popup').removeClass('hidden');
+});
+
+$(".info-box__close").click(function(e) {
+		e.preventDefault();
+		$(this).parent().addClass('hidden');
+});
+
+$(".aside-menu__item").click(function(e) {
+		e.preventDefault();
+		$(this).toggleClass('active');
+});
+
+$(function() {
+  var caption = $(".mail-list__item").find(".mail-trigger"),
+    content = $(".mail-list__item").find(".mail-list__item-content").hide();
+
+  caption.click(function(e) {
+    e.preventDefault();
+    var activeContent = content.filter('.active');
+    activeContent.removeClass('active').slideUp(600);
+    if ($(this).next(content).is(activeContent)) return;
+    $(this).next(content).addClass('active').slideDown(300);
+  });
+});
+
+
+
+
+
+// ***** Add bootstrap triggers *****
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+});
+
+
+
+
+
+// ***** Plugin's init and optionated *****
 $('.select__department-trigger, .dicom-options__head').chosen({
 	inherit_select_classes: true,
 	disable_search: true
@@ -39,13 +106,13 @@ $('.select__add-element').multipleSelect({
 	placeholder: "Выберите что нибудь",
 	onUncheckAll: true
 });
+
 $('.ms-drop input').on('click', function(){
 	if($(this).prop("checked")){
 		$(this).addClass('checkbox__marked');
 	};
 	$(this).parent().parent('li').toggleClass('checkbox__marked');
 });
-
 
 // $("#refreshAdd").click(function() {
 //     var $select = $("select"),
@@ -62,40 +129,6 @@ $('.ms-drop input').on('click', function(){
 //     $('.ms-drop ul').append($('.form__add-element'));
 // });
 	
-
-$('.row-phone .js__delete-row').on('click', function(){
-	$(this).parent('.row-phone').hide();
-});
-
-$('.js__add-row').on('click', function(e){
-	e.preventDefault();
-	// var innerRow = $(this).parent().parent($('.row-phone'));
-	var innerH = $('.fieldset').append($('.row-phone').html());
-});
-
-
-
-$(".info-box__trigger").click(function(e) {
-		e.preventDefault();
-		$(this).siblings('.info-box').removeClass('hidden');
-});
-$(".js-call__popup").click(function(e) {
-		e.preventDefault();
-		$(this).siblings('.cms__widget-popup').removeClass('hidden');
-});
-
-$(".info-box__close").click(function(e) {
-		e.preventDefault();
-		$(this).parent().addClass('hidden');
-});
-
-
-
-$(".aside-menu__item").click(function(e) {
-		e.preventDefault();
-		$(this).toggleClass('active');
-});
-
 $(".js-pnotify").click(function(){
     PNotify.prototype.options.styling = "fontawesome";
 	new PNotify({
@@ -108,20 +141,6 @@ $(".js-pnotify").click(function(){
 	    stack: {"dir1": "down", "dir2": "left", "push": "top", "firstpos1": 110, "firstpos2": 0}
 	});
 })
-
-    $(function() {
-      var caption = $(".mail-list__item").find(".mail-trigger"),
-        content = $(".mail-list__item").find(".mail-list__item-content").hide();
-
-      caption.click(function(e) {
-        e.preventDefault();
-        var activeContent = content.filter('.active');
-        activeContent.removeClass('active').slideUp(600);
-        if ($(this).next(content).is(activeContent)) return;
-        $(this).next(content).addClass('active').slideDown(300);
-      });
-    });
-
 
 new Chartist.Line('#graphic', {
   labels: ['01.04.2015', '01.05.2015', '01.06.2015', '01.07.2015', '01.08.2015', '01.09.2015', '01.10.2015', '01.11.2015'],
@@ -141,11 +160,3 @@ new Chartist.Line('#graphic', {
 
 
 
-
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-});
-
-$(function () {
-  $('[data-toggle="popover"]').popover()
-});
